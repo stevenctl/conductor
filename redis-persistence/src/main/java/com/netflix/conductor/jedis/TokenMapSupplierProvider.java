e3a63f8ec160f37aa60c92806c60f78619e7d453
Steven Landow <greenpipestudios@gmail.com>
2018-10-24 18:02:40 -0500

    use built-in dynomite cluster_describe to get tokens

--- redis-persistence/src/main/java/com/netflix/conductor/jedis/TokenMapSupplierProvider.java
+++ redis-persistence/src/main/java/com/netflix/conductor/jedis/TokenMapSupplierProvider.java
@@ -2,6 +2,7 @@ package com.netflix.conductor.jedis;
 
 import com.google.common.collect.Lists;
 
+import com.netflix.conductor.dyno.DynomiteConfiguration;
 import com.netflix.dyno.connectionpool.Host;
 import com.netflix.dyno.connectionpool.HostSupplier;
 import com.netflix.dyno.connectionpool.TokenMapSupplier;
@@ -17,17 +18,25 @@ import javax.inject.Provider;
 public class TokenMapSupplierProvider implements Provider<TokenMapSupplier> {
     private final HostSupplier hostSupplier;
 
+    private final DynomiteConfiguration configuration;
+
     @Inject
-    public TokenMapSupplierProvider(HostSupplier hostSupplier) {
+    public TokenMapSupplierProvider(HostSupplier hostSupplier, DynomiteConfiguration configuration) {
         this.hostSupplier = hostSupplier;
+        this.configuration = configuration;
     }
 
     @Override
     public TokenMapSupplier get() {
+
+        // If we specified the admin port for our dynomite servers, use that to get the hosts' tokens
+        int dynomiteAdminPort = configuration.getDynomiteAdminPort();
+        if(dynomiteAdminPort > -1) {
+            return new DynomiteTokenMapSupplier(dynomiteAdminPort);
+        }
+
         return new TokenMapSupplier() {
 
-            // FIXME This isn't particularly safe, but it is equivalent to the existing code.
-            // FIXME It seems like we should be supply tokens for more than one host?
             HostToken token = new HostToken(1L, Lists.newArrayList(hostSupplier.getHosts()).get(0));
 
             @Override
